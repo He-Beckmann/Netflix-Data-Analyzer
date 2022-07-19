1f79cdd6924b56ec5d6d57bf01d59eeefca44b47
beckmann.hendrik <beckmann.hendrik@gmx.de>
2022-07-19 20:14:18 +0200

    Added Button

--- NetflixAnalyzer.py
+++ NetflixAnalyzer.py
@@ -1,44 +1,47 @@
 import sys
-from PyQt5.QtWidgets import (QWidget, QPushButton, QLineEdit, QInputDialog, QApplication, QLabel)
+from PyQt5.QtWidgets import (QWidget, QPushButton, QLineEdit, QInputDialog, QApplication, QLabel, QFileDialog)
+
+class NetflixAnalyzer:
+
+    def __init__(self):
+
+        app = QApplication(sys.argv)
+        self.widget = QWidget()
+        self.widget.resize(300,300)
+        self.widget.setWindowTitle("Netflix Analyzer")
+        
+        self.displayFolderInputScreen()
+        
+        self.widget.show()
+        sys.exit(app.exec_())
 
 
-def dialog():
-    mbox = QMessageBox()
+    def displayFolderInputScreen(self):
+        
+        label = QLabel(self.widget)
+        label.setText("Behold the Guru, Guru99")
+        label.move(100,130)
+        label.show()
 
-    mbox.setText("Your allegiance has been noted")
-    mbox.setDetailedText("You are now a disciple and subject of the all-knowing Guru")
-    mbox.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
-            
-    mbox.exec_()
+        btn = QPushButton(self.widget)
+        btn.setText('Select Folder')
+        btn.move(110,150)
+        btn.show()
+        # btn.clicked.connect('test')
+
+        # label = QLabel(w)
+        # label.setText("Behold the Guru, Guru99")
+        # label.move(100,130)
+        # label.show()
+
+
+
+        # text = QFileDialog.getExistingDirectory(w, 'Select Folder')
+
+
+
+
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    w = QWidget()
-    w.resize(300,300)
-    w.setWindowTitle("Guru99")
-
-
-    label = QLabel(w)
-    label.setText("Behold the Guru, Guru99")
-    label.move(100,130)
-    label.show()
-
-    btn = QPushButton(w)
-    btn.setText('Beheld')
-    btn.move(110,150)
-    btn.show()
-    btn.clicked.connect(dialog)
-
-
-    setGeometry(300, 300, 290, 150)
-    setWindowTitle('Input dialog')
-    show()
-
-
-    w.show()
-    sys.exit(app.exec_())
-
-
-
-
+    app = NetflixAnalyzer()
 
