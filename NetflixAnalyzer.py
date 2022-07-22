ee93424c8b5bbf783e29871d0474fd66175f7ac6
beckmann.hendrik <beckmann.hendrik@gmx.de>
2022-07-22 23:02:04 +0200

    Update File Select

--- NetflixAnalyzer.py
+++ NetflixAnalyzer.py
@@ -4,41 +4,53 @@ from PyQt5.QtWidgets import (QWidget, QPushButton, QLineEdit, QInputDialog, QApp
 class NetflixAnalyzer:
 
     def __init__(self):
-
+        self.folderPath = ''
         app = QApplication(sys.argv)
         self.widget = QWidget()
-        self.widget.resize(300,300)
+        self.widget.setFixedWidth(400)
+        self.widget.setFixedHeight(200)
         self.widget.setWindowTitle("Netflix Analyzer")
         
         self.displayFolderInputScreen()
-        
+
         self.widget.show()
         sys.exit(app.exec_())
 
 
     def displayFolderInputScreen(self):
         
-        label = QLabel(self.widget)
-        label.setText("Behold the Guru, Guru99")
-        label.move(100,130)
-        label.show()
+        lineHeight = 80
+        
+        nameLabel = QLabel(self.widget)
+        nameLabel.setText('Name:')
+        self.line = QLineEdit(self.widget)
+
+        self.line.move(80, lineHeight)
+        self.line.resize(200, 20)
+        self.line.returnPressed.connect(self.selectFolder)
+        # line.setReadOnly(True)
+        nameLabel.move(20, lineHeight)
 
         btn = QPushButton(self.widget)
         btn.setText('Select Folder')
-        btn.move(110,150)
+        btn.move(300, lineHeight)
         btn.show()
-        text = btn.clicked.connect(self.selectFolder)
-        print(text)
-
-
+        btn.clicked.connect(self.selectFolder)
+        
+        btn = QPushButton(self.widget)
+        btn.setText('Analyze Netflix Account')
+        btn.move(150, 120)
+        btn.show()
+        btn.clicked.connect(self.selectFolder)
+        
+        self.line.setText(self.folderPath)
 
 
         # text = QFileDialog.getExistingDirectory(w, 'Select Folder')
     def selectFolder(self):
-        text = QFileDialog.getExistingDirectory(self.widget, 'Select Folder')
-        # print(text)
-        return False
-
+        self.folderPath = QFileDialog.getExistingDirectory(self.widget, 'Select Folder')
+        self.line.setText(self.folderPath)
+        return self.folderPath
 
 
 if __name__ == '__main__':
